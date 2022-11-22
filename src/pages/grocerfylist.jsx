5fc2a79550333eccc527eb9089421fa7ffee3f51
Fabian Juren <fjuren@gmail.com>
2022-11-22 14:58:03 -0800

    Add text bar and add button to grocerfylist page

--- src/pages/grocerfylist.jsx
+++ src/pages/grocerfylist.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import theme from '../theme';
+import '../assets/styles/grocerfylist.css';
+
+import Input from '@mui/material/Input';
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+// import { auth, db } from '../firebase.config';
 
 const Grocerfylist = () => {
+  const ariaLabel = { 'aria-label': 'description' };
   return (
     <div id="container-grocerfy-page">
-      <ThemeProvider theme={theme}></ThemeProvider>
-      <h1>My Grocerfy List</h1>
+      <ThemeProvider theme={theme}>
+        <h1>My Grocerfy List</h1>
+        <div>
+          <Input placeholder="Add ingredient" inputProps={ariaLabel} />
+          <Button sx={{ width: '6rem' }} variant="contained" startIcon={<AddIcon />}>
+            Add
+          </Button>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
