d7c72a7943fbc58154916849bfac5f885be06224
Fabian Juren <fjuren@gmail.com>
2022-11-23 09:31:06 -0800

    create groceries db in firestore

--- src/pages/grocerfylist.jsx
+++ src/pages/grocerfylist.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import theme from '../theme';
 import '../assets/styles/grocerfylist.css';
@@ -7,19 +7,47 @@ import Input from '@mui/material/Input';
 import { Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
-// import { auth, db } from '../firebase.config';
+import { auth, db } from '../firebase.config';
+import { addDoc, collection } from 'firebase/firestore';
 
 const Grocerfylist = () => {
+  // const [checkmark, setCheckmark] = useState(false);
+  const [ingredient, setIngredient] = useState('');
+
   const ariaLabel = { 'aria-label': 'description' };
+
+  const groceriesCollection = collection(db, 'groceries');
+
+  const handleIngredient = (e) => {
+    setIngredient(e.target.value);
+  };
+
+  const addIngredient = async () => {
+    await addDoc(groceriesCollection, {
+      ingredient,
+      author: { id: auth.currentUser.uid, name: auth.currentUser.displayName }
+    });
+  };
+
   return (
     <div id="container-grocerfy-page">
       <ThemeProvider theme={theme}>
-        <h1>My Grocerfy List</h1>
-        <div>
-          <Input placeholder="Add ingredient" inputProps={ariaLabel} />
-          <Button sx={{ width: '6rem' }} variant="contained" startIcon={<AddIcon />}>
-            Add
-          </Button>
+        <h1 className="title">My Grocerfy List</h1>
+        <div className="grocerfyList-container">
+          <div className="addIngredients-container">
+            <Input
+              placeholder="Add ingredient"
+              inputProps={ariaLabel}
+              onChange={(e) => handleIngredient(e)}
+            />
+            <Button
+              sx={{ width: '6rem' }}
+              variant="contained"
+              startIcon={<AddIcon />}
+              onClick={addIngredient}>
+              Add
+            </Button>
+          </div>
         </div>
       </ThemeProvider>
     </div>
