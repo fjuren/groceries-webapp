d3823cd6f56485baa255552af7f8cbcf9d21d38a
Fabian Juren <fjuren@gmail.com>
2022-12-08 17:36:15 -0800

    Building create-recipe page

--- src/pages/subpages/createNewRecipe.jsx
+++ src/pages/subpages/createNewRecipe.jsx
@@ -1,4 +1,11 @@
 import Breadcrumb from '../../components/breadcrumb';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../../theme';
+import '../../assets/styles/createnewrecipe.css';
 // import { useNavigate } from 'react-router-dom';
 
 const Createnewrecipe = () => {
@@ -7,14 +14,81 @@ const Createnewrecipe = () => {
   const parentPageName = 'Recipes';
   const currentPageName = 'Create a recipe';
 
+  const handleTitle = (e) => {
+    e.preventDefaul();
+  };
+
+  const handleDescription = (e) => {
+    e.preventDefaul();
+  };
+
   return (
-    <div>
-      <Breadcrumb
-        parentPageName={parentPageName}
-        parentPage={parentPage}
-        currentPageName={currentPageName}
-      />
-      <h1>Create new recipe here</h1>
+    <div id="container-create-recipe-page">
+      <ThemeProvider theme={theme}>
+        <div>
+          <Breadcrumb
+            parentPageName={parentPageName}
+            parentPage={parentPage}
+            currentPageName={currentPageName}
+          />
+        </div>
+        <div id="container-create-recipe-form">
+          <div>
+            <div>
+              <h2>Create your recipe</h2>
+              <p>
+                Create a recipe, save it, share it, and take a list of ingredients with you to the
+                grocery store!
+              </p>
+            </div>
+            <div>
+              {/* <form onSubmit={(e) => addItem(e)}> */}
+              <form id="form-create-recipe">
+                <Stack spacing={2}>
+                  <div>
+                    <TextField
+                      fullWidth
+                      id="title"
+                      label="Recipe Title"
+                      variant="outlined"
+                      onChange={(e) => handleTitle(e)}
+                    />
+                  </div>
+                  <div>
+                    <TextField
+                      fullWidth
+                      id="description"
+                      label="Recipe Description"
+                      multiline
+                      rows={4}
+                      variant="outlined"
+                      onChange={(e) => handleDescription(e)}
+                    />
+                  </div>
+                  <div>
+                    <Button
+                      sx={{ width: '6rem' }}
+                      variant="outlined"
+                      startIcon={<AddIcon />}
+                      // onClick={(e) => addItem(e)}
+                      type="submit">
+                      Add
+                    </Button>
+                  </div>
+                  <Button
+                    sx={{ width: '6rem' }}
+                    variant="contained"
+                    // startIcon={<AddIcon />}
+                    // onClick={(e) => addItem(e)}
+                    type="submit">
+                    Save
+                  </Button>
+                </Stack>
+              </form>
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
